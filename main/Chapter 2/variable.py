864e970edab728f71aa1af9a13b4c5c3c22442c9
NeoIgrix <dakshchaudhary48144@gmail.com>
2026-01-15 16:04:50 +0530

    .

---
+++ main/Chapter 2/variable.py
@@ -0,0 +1,6 @@
+a= 71         # identifies a as class <int> 
+b=88.44       # identifies b as class <float> 
+name = "harry" # identifies name as class <str> 
+d = False # boolean variable
+e = None # none variable ie kuch nahi he 
+_name = "daksh" # valid variable he
